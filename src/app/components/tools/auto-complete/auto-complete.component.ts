14bb92729bfa3f19a9829878e34b7732f0afd1ed
Antonio Ibarra <elkelvin484@gmail.com>
2024-02-22 21:49:08 -0700

    Setting the div sercher at units

--- src/app/components/tools/auto-complete/auto-complete.component.ts
+++ src/app/components/tools/auto-complete/auto-complete.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { debounceTime, map, startWith } from 'rxjs/operators';
