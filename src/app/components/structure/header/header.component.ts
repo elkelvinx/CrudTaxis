a2137e0bf2d6c1bede273b9309d1ff484f5c3137
Antonio Ibarra <elkelvin484@gmail.com>
2024-06-15 01:55:47 -0700

    boton LogOut

--- src/app/components/structure/header/header.component.ts
+++ src/app/components/structure/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AppRoutingModule } from '../../../app-routing.module';
 import { AuthService } from '../../../services/security/authService.service';
 import { Router } from '@angular/router';
+import {DialogAnimationsExampleDialog} from '../../tools/yes-no-dialog/yes-no-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +16,7 @@ export class HeaderComponent {
   menuWidth = '70px'; // Ancho inicial
   headerWidth = '3.7%'; // Ancho inicial
   iconButtonWidth = '1%'; 
-  constructor(private router: Router,private auth:AuthService) { }
+  constructor(private router: Router,private auth:AuthService,public dialog: MatDialog) { }
 
   isActive(path: string): boolean {
     const currentRoute = this.router.url;
@@ -46,6 +48,24 @@ closeSession(){
     this.headerWidth = '3.7%'
     this.iconButtonWidth = '1%'; 
     this.isMenuOpen = false;
+  }
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string, contentDialog: string): void {
+    const dialogRef = this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '530px',
+      height: '180px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+      data: {
+        contentDialog: contentDialog,
+        nameObj: name,
+        title: 'Cerrar Sesión',
+      },
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.closeSession();
+      }
+    });
   }
 
 }
